15be68b49091a011e6e459f8054743ee1f6d2384
mantisatheus <mantisatheus@gmail.com>
2016-09-12 23:54:36 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+Notification center v0.5
+
+needs angularJs 1.4 or >
+
+
+add 
+	mantis_notify.js, mantis-noyify.css
+
+add 
+	<notifications max="3"></notifications>
+
+add 
+
+	'mantisNotify' in angular module	
+
+configure example:
+
+	myModule.config(['NotifyProvider', function(NotifyProvider) {
+
+	    NotifyProvider.create('error',{template:'error.html'},function(text,time) {
+	        this.text=text || 'some default error';
+	        this.timeout=time || 10000;
+	    });
+
+	}]);
+
+	-- this creates new notify type named 'error',
+
+use example:
+
+	Notify.error(['some error'],1000);
+
+template example 'error.html':
+
+	<a href="" role="button" ng-click="notification.close(id)">
+	    close
+	</a>
+
+	<p>{{::notification.text}}</p>
+
+
+
+
---
+++ mantis-notify.js
@@ -0,0 +1,73 @@
+// Notification center v0.5
+
+//
+
+(function() {
+'use strict';
+
+    var notifyModule = angular.module('mantisNotify', []);
+
+    notifyModule.provider('Notify',function() {
+
+        this.notifications=[];
+
+        this.create=function(name,obj,fn) {
+
+            var newN=function() {
+                var notification=angular.copy(obj);
+                notification.show=true;
+                notification.timeout=notification.timeout || 6000;
+                notification.template=notification.template || '';
+                notification.text=notification.text || '';
+                notification.close=function(id) {
+                    n.notifications.splice(id,1);
+                };
+                if (typeof fn == 'function') {fn.apply(notification,arguments)};
+                notification.delay=this.$timeout(function() { 
+                    notification.show=false;
+                },notification.timeout);
+                this.notifications.push(notification);
+                this.killOld();
+            };
+
+            this[name]=newN;
+        };
+
+        this.Clear=function() {
+            this.notifications=[];
+        };
+
+        this.killOld=function() {
+            if (this.notifications.length > this.maxLength) {
+                this.notifications.shift();
+            };    
+        };
+
+        this.$get=['$timeout',function($timeout) {
+            this.$timeout=$timeout;
+            return this;
+        }];
+
+    });
+
+    notifyModule.directive('notifications', function (Notify) {
+        var dirDefObj={
+            replace:true,
+            template:
+            '<div class="notifications">'+
+                '<div class="notificationStack" ng-if="notifications.length">'+
+                    '<div class="alert alert-success" role="alert" ng-repeat="(id,notification) in notifications" ng-include="notification.template" ng-if="notification.show">'+
+                    '</div>'+
+                '</div>'+
+            '</div>',
+            link: function ($scope, el, attrs) {
+                $scope.notifications=Notify.notifications;
+                Notify.maxLength=+attrs.max || 3;
+            }
+        };
+        return dirDefObj;    
+    });
+
+
+}());
+
